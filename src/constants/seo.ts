07295cf76e8ad27b0dfa53a12308a2dec36b06d8
archaon <archaon@generative.xyz>
2023-04-27 15:04:05 +0700

    feat: update seo info

--- src/constants/seo.ts
+++ src/constants/seo.ts
@@ -3,4 +3,4 @@ import { CDN_URL } from '@/configs';
 export const SEO_TITLE = 'Trustless NFTs';
 export const SEO_DESCRIPTION =
   'Issue your BRC-721 NFTs on Bitcoin for collectible items, memberships, in-game items, and more.';
-export const SEO_IMAGE = `${CDN_URL}/images/metadata-Trustless-NFTs.jpg`;
+export const SEO_IMAGE = `${CDN_URL}/images/metadata-NFTs.png`;
--- src/pages/collection.tsx
+++ src/pages/collection.tsx
@@ -1,6 +1,9 @@
-import { NextPage } from 'next';
+import { GetServerSidePropsContext, NextPage } from 'next';
 import Layout from '@/layouts';
 import CollectionDetail from '@/containers/CollectionDetail';
+import { getCollectionDetail } from '@/services/nft-explorer';
+import { ROUTE_PATH } from '@/constants/route-path';
+import { SEO_DESCRIPTION, SEO_IMAGE, SEO_TITLE } from '@/constants/seo';
 
 const CollectionDetailPage: NextPage = () => {
   return (
@@ -11,3 +14,29 @@ const CollectionDetailPage: NextPage = () => {
 };
 
 export default CollectionDetailPage;
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const { query } = context;
+  const { contract } = query as { contract: string };
+
+  try {
+    const data = await getCollectionDetail({ contractAddress: contract });
+
+    return {
+      props: {
+        seoInfo: {
+          title: `${SEO_TITLE} | ${data.name} `,
+          description: data.description || SEO_DESCRIPTION,
+          image: data.thumbnail || SEO_IMAGE,
+        },
+      },
+    };
+  } catch (err: unknown) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: ROUTE_PATH.NOT_FOUND,
+      },
+    };
+  }
+}
