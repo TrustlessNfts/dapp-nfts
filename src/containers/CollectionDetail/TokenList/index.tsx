26d96896e38ce07bf5a521c9a9bf01c53d66f81d
archaon <archaon@generative.xyz>
2023-06-12 10:10:52 +0700

    fix: remove make offer column

--- src/containers/CollectionDetail/TokenList/index.tsx
+++ src/containers/CollectionDetail/TokenList/index.tsx
@@ -1,25 +1,24 @@
-import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
-import { Wrapper } from './TokenList.styled';
-import { getCollectionNFTList } from '@/services/marketplace';
-import logger from '@/services/logger';
-import InfiniteScroll from 'react-infinite-scroll-component';
-import { ICollection, IToken } from '@/interfaces/api/marketplace';
-import { Spinner } from 'react-bootstrap';
 import Empty from '@/components/Empty';
 import ImageWrapper from '@/components/ImageWrapper';
-import { TC_EXPLORER } from '@/constants/url';
-import { shortenAddress } from '@trustless-computer/dapp-core';
-import Link from 'next/link';
-import { formatEthPrice, mappingERC20ToIcon } from '@/utils/format';
 import ModalPurchase from '@/components/Transactor/ModalPurchase';
-import ModalMakeOffer from '@/components/Transactor/ModalMakeOffer';
-import { IInscription } from '@/interfaces/api/inscription';
-import { useSelector } from 'react-redux';
-import { getUserSelector } from '@/state/user/selector';
 import { ROUTE_PATH } from '@/constants/route-path';
+import { TC_EXPLORER } from '@/constants/url';
+import { IInscription } from '@/interfaces/api/inscription';
+import { ICollection, IToken } from '@/interfaces/api/marketplace';
+import logger from '@/services/logger';
+import { getCollectionNFTList } from '@/services/marketplace';
+import { getUserSelector } from '@/state/user/selector';
+import { formatEthPrice, mappingERC20ToIcon } from '@/utils/format';
 import { showToastError } from '@/utils/toast';
+import { shortenAddress } from '@trustless-computer/dapp-core';
 import uniqBy from 'lodash/uniqBy';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import { Spinner } from 'react-bootstrap';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { useSelector } from 'react-redux';
+import { Wrapper } from './TokenList.styled';
 
 const FETCH_LIMIT = 32;
 
@@ -33,49 +32,31 @@ const TokenList: React.FC<IProps> = ({ collection }: IProps): React.ReactElement
   const [nftList, setNftList] = useState<Array<IToken>>([]);
   const [loading, setLoading] = useState(true);
   const [showPurchase, setShowPurchase] = useState(false);
-  const [showMakeOffer, setShowMakeOffer] = useState(false);
   const [selectedToken, setSelectedToken] = useState<IToken | null>(null);
 
   const handleOpenPurchase = (data: IToken) => {
     if (!user.walletAddress) {
       router.push(ROUTE_PATH.CONNECT_WALLET);
       showToastError({
-        message: 'Please connect wallet to continue.'
-      })
+        message: 'Please connect wallet to continue.',
+      });
       return;
     }
     setShowPurchase(true);
     setSelectedToken(data);
-  }
+  };
 
   const handleClosePurchase = () => {
     setShowPurchase(false);
     setSelectedToken(null);
-  }
-
-  const handleOpenMakeOffer = (data: IToken) => {
-    if (!user.walletAddress) {
-      router.push(ROUTE_PATH.CONNECT_WALLET);
-      showToastError({
-        message: 'Please connect wallet to continue.'
-      })
-      return;
-    }
-    setShowMakeOffer(true);
-    setSelectedToken(data);
-  }
-
-  const handleCloseMakeOffer = () => {
-    setShowMakeOffer(false);
-    setSelectedToken(null);
-  }
+  };
 
   const fetchNFTList = async (p?: number): Promise<void> => {
     if (!collection) return;
 
     try {
       setLoading(true);
-      const page = p || (Math.floor(nftList.length / FETCH_LIMIT) + 1);
+      const page = p || Math.floor(nftList.length / FETCH_LIMIT) + 1;
       const res = await getCollectionNFTList({
         contract_address: collection.contract,
         page: page,
@@ -85,7 +66,7 @@ const TokenList: React.FC<IProps> = ({ collection }: IProps): React.ReactElement
       if (page === 1) {
         setNftList(res);
       } else {
-        setNftList(prev => uniqBy([...prev, ...res], 'tokenId'));
+        setNftList((prev) => uniqBy([...prev, ...res], 'tokenId'));
       }
     } catch (err: unknown) {
       logger.error(err);
@@ -99,84 +80,97 @@ const TokenList: React.FC<IProps> = ({ collection }: IProps): React.ReactElement
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const hasMore = !!collection && (nftList.length < collection.totalItems);
+  const hasMore = !!collection && nftList.length < collection.totalItems;
 
   return (
     <>
       <Wrapper>
         <InfiniteScroll
-          className='disable-scrollbar'
+          className="disable-scrollbar"
           hasMore={hasMore}
           dataLength={nftList.length}
           next={fetchNFTList}
           height={600}
           style={{ overflow: 'hidden auto' }}
-          loader={loading ?
-            (
+          loader={
+            loading ? (
               <div className="loading-wrapper">
-                <Spinner variant='light' />
+                <Spinner variant="light" />
               </div>
-            ) : <></>
+            ) : (
+              <></>
+            )
           }
         >
-          {(!loading && nftList.length === 0) && (
-            <Empty />
-          )}
+          {!loading && nftList.length === 0 && <Empty />}
 
           {nftList.length > 0 && (
-            <table className='data-table'>
+            <table className="data-table">
               <thead>
                 <tr>
-                  <th>
-                    {`Items`}
-                  </th>
-                  <th>
-                    Owner
-                  </th>
-                  <th>
-                    Buy now
-                  </th>
-                  <th>
-                    Offer
-                  </th>
+                  <th>{`Items`}</th>
+                  <th>Owner</th>
+                  <th>Buy now</th>
                 </tr>
               </thead>
               <tbody>
                 {nftList.map((token: IToken) => {
                   return (
                     <tr key={token.tokenId}>
-                      <td className='token-info'>
+                      <td className="token-info">
                         <div className="token-info-wrapper">
                           <div className="thumbnail-wrapper">
-                            <Link href={`${ROUTE_PATH.COLLECTION}/${token.collectionAddress}/token/${token.tokenId}`}>
-                              <ImageWrapper className='token-thumbnail' src={token.image} alt={collection?.name} />
+                            <Link
+                              href={`${ROUTE_PATH.COLLECTION}/${token.collectionAddress}/token/${token.tokenId}`}
+                            >
+                              <ImageWrapper
+                                className="token-thumbnail"
+                                src={token.image}
+                                alt={collection?.name}
+                              />
                             </Link>
                           </div>
                           <div className="token-info">
-                            <Link href={`${ROUTE_PATH.COLLECTION}/${token.collectionAddress}/token/${token.tokenId}`} className='token-id'>{`#${token.tokenId}`}</Link>
+                            <Link
+                              href={`${ROUTE_PATH.COLLECTION}/${token.collectionAddress}/token/${token.tokenId}`}
+                              className="token-id"
+                            >{`#${token.tokenId}`}</Link>
                           </div>
                         </div>
                       </td>
-                      <td className='token-owner'>
-                        <Link href={`${TC_EXPLORER}/address/${token.owner}`} target='_blank' className='token-owner-link'>{shortenAddress(token.owner)}</Link>
+                      <td className="token-owner">
+                        <Link
+                          href={`${TC_EXPLORER}/address/${token.owner}`}
+                          target="_blank"
+                          className="token-owner-link"
+                        >
+                          {shortenAddress(token.owner)}
+                        </Link>
                       </td>
-                      <td className='buy-now'>
-                        {(token.buyable && token.priceErc20.price && token.priceErc20.erc20Token && token.priceErc20.offeringId && user?.walletAddress?.toLowerCase() !== token.owner.toLowerCase()) && (
-                          <button className='purchase-btn' onClick={() => handleOpenPurchase(token)}>
-                            <span>{`${formatEthPrice(token.priceErc20.price)}`}</span>
-                            <img className='token-icon' src={mappingERC20ToIcon(token.priceErc20.erc20Token)} alt="coin-ic" />
-                          </button>
-                        )}
-                      </td>
-                      <td className='make-offer'>
-                        {user?.walletAddress?.toLowerCase() !== token.owner.toLowerCase() && (
-                          <button className='make-offer-btn' onClick={() => handleOpenMakeOffer(token)}>
-                            Make offer
-                          </button>
-                        )}
+                      <td className="buy-now">
+                        {token.buyable &&
+                          token.priceErc20.price &&
+                          token.priceErc20.erc20Token &&
+                          token.priceErc20.offeringId &&
+                          user?.walletAddress?.toLowerCase() !==
+                            token.owner.toLowerCase() && (
+                            <button
+                              className="purchase-btn"
+                              onClick={() => handleOpenPurchase(token)}
+                            >
+                              <span>{`${formatEthPrice(
+                                token.priceErc20.price,
+                              )}`}</span>
+                              <img
+                                className="token-icon"
+                                src={mappingERC20ToIcon(token.priceErc20.erc20Token)}
+                                alt="coin-ic"
+                              />
+                            </button>
+                          )}
                       </td>
                     </tr>
-                  )
+                  );
                 })}
               </tbody>
             </table>
@@ -189,14 +183,8 @@ const TokenList: React.FC<IProps> = ({ collection }: IProps): React.ReactElement
         inscription={selectedToken as unknown as IInscription}
         handleClose={handleClosePurchase}
       />
-      <ModalMakeOffer
-        show={showMakeOffer}
-        inscription={selectedToken as unknown as IInscription}
-        handleClose={handleCloseMakeOffer}
-      />
-
     </>
-  )
-}
+  );
+};
 
 export default React.memo(TokenList);
