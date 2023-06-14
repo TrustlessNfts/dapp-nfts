5962d3e16dba39b41d9ce8fa8e2391ba7aa72738
benjiro2077 <benjiro2077@generative.xyz>
2023-06-14 17:54:42 +0700

    feat: default image

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,3 +1,6 @@
+import { CDN_URL } from "@/configs";
+
 export const ROOT_ADDRESS = '0x0000000000000000000000000000000000000000';
 export const MAX_HEX_VALUE =
   '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff';
+export const PLACEHOLDER_IMAGE = `${CDN_URL}/images/dapp-nft.png`;
